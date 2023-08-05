788bfd266059b396043ca2c00b0b5bae0c28490b
Coderships <91427392+Coderships@users.noreply.github.com>
2023-08-05 23:52:23 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+const btn1=document.getElementById('btn1');
+btn1.addEventListener('click',()=>{
+    alert("Congratulataions!!🥳 Your Email has been sent successfully and you are going to avail 39% discount on any products.")
+})
